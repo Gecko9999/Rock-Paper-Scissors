1227ee730c9b548146e6fdda183f7b12cb3039f5
Matija <cresnik.matija@gmail.com>
2022-10-06 19:41:51 +0200

    Use identation to make the code clear

--- app.js
+++ app.js
@@ -1,52 +1,43 @@
 
-let player = 0
-let computer = 0
+let playerScore = 0
+let computerScore = 0
+
+// randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 
 function getComputerChoice(){
-// randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 const options = ['rock', 'paper', 'scissors']
 const selection = options[Math.floor(Math.random()*options.length)]
 return selection
 }
 
 function playRound(playerSelection, computerSelection){
-
 if (playerSelection === computerSelection) {
-let message1 = 'It\'s a draw.'
-return message1
-}
-else if (playerSelection === 'rock' && computerSelection === 'scissors' ||
-    playerSelection === 'paper' && computerSelection === 'rock' ||
-    playerSelection === 'scissors' && computerSelection === 'paper'){
-    let message2 = `You win, ${playerSelection} beats ${computerSelection}!`
-    player ++
-    return message2
+return 'It\'s a draw.'
+}else if (playerSelection === 'rock' && computerSelection === 'scissors' ||
+playerSelection === 'paper' && computerSelection === 'rock' ||
+playerSelection === 'scissors' && computerSelection === 'paper'){
+playerScore ++
+return `You win, ${playerSelection} beats ${computerSelection}!`   
 }else {
-    let message3 = `You lose, ${computerSelection} beats ${playerSelection}!`
-    computer ++
-    return message3
+computerScore++
+return`You lose, ${computerSelection} beats ${playerSelection}!`   
+    }
 }
-}
-
-
 
 function game(){
-    
-   for (let i = 0; i < 5; i++){
-    
- const playerSelection = prompt('Choose between rock, paper or scissors').toLowerCase()
- console.log(playRound(playerSelection,getComputerChoice()))
-
+for (let i = 0; i < 5; i++){   
+const playerSelection = prompt('Choose between rock, paper or scissors').toLowerCase()
+console.log(playRound(playerSelection,getComputerChoice()))
    }
 }
 
 game()
 
 function trackScore() {
-    if (player > computer) {
-        console.log(`**ATTENTION** YOU WON! You scored ${player} while this stupid machine scored ${computer}.`)
-    }else if (player < computer){
-        console.log(`**ATTENTION** YOU LOST, TRY AGAIN! You scored ${player} while computer beat you with the score of ${computer}.`)
-    }
+if (playerScore > computerScore) {
+console.log(`**ATTENTION** YOU WON! You scored ${playerScore} while this stupid machine scored ${computerScore}.`)
+}else if (playerScore < computerScore){
+console.log(`**ATTENTION** YOU LOST, TRY AGAIN! You scored ${playerScore} while computer beat you with the score of ${computerScore}.`)
+    }else {console.log(`**ATTENTION** After the 5 rounds you tied with the computer!`)}
 }
 trackScore()
