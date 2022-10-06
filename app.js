fd080c2652cb49e98088ebd7843537f7e2c7abce
Matija <cresnik.matija@gmail.com>
2022-10-06 16:38:43 +0200

    Create game logic and track the score of each round.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const playerSelection = prompt('Choose between rock, paper or scissors').toLowerCase()
 
+let player = 0
+let computer = 0
 
 function getComputerChoice(){
 // randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 const options = ['rock', 'paper', 'scissors']
-const selection = options[Math.floor(Math.random()*options.length)].toLowerCase()
+const selection = options[Math.floor(Math.random()*options.length)]
 return selection
 }
 
@@ -18,20 +19,34 @@ else if (playerSelection === 'rock' && computerSelection === 'scissors' ||
     playerSelection === 'paper' && computerSelection === 'rock' ||
     playerSelection === 'scissors' && computerSelection === 'paper'){
     let message2 = `You win, ${playerSelection} beats ${computerSelection}!`
+    player ++
     return message2
 }else {
     let message3 = `You lose, ${computerSelection} beats ${playerSelection}!`
+    computer ++
     return message3
 }
 }
 
-// console.log(playRound(playerSelection,getComputerChoice()))
+
 
 function game(){
+    
    for (let i = 0; i < 5; i++){
-const games = playRound(playerSelection,getComputerChoice())
+    
+ const playerSelection = prompt('Choose between rock, paper or scissors').toLowerCase()
+ console.log(playRound(playerSelection,getComputerChoice()))
 
    }
 }
 
 game()
+
+function trackScore() {
+    if (player > computer) {
+        console.log(`**ATTENTION** YOU WON! You scored ${player} while this stupid machine scored ${computer}.`)
+    }else if (player < computer){
+        console.log(`**ATTENTION** YOU LOST, TRY AGAIN! You scored ${player} while computer beat you with the score of ${computer}.`)
+    }
+}
+trackScore()
